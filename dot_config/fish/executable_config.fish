6cff4c39d4e26f3fb31f7ea472b5058560187b83
Martin Pittermann <martinp2250@gmail.com>
2020-06-21 16:47:22 +0200

    update fish

--- dot_config/fish/executable_config.fish
+++ dot_config/fish/executable_config.fish
@@ -26,3 +26,16 @@ alias gpu='git push'
 # clipboard actions
 alias clipget='xclip -selection c -o'	# get clipboard to stdout
 alias clipset='xclip -selection c'		# set clipboard from stdin
+
+function run
+	$argv > /dev/null 2>&1 &
+	disown > /dev/null 2>&1;
+end
+
+function mkcd
+	mkdir "$argv";
+	cd "$argv";
+end
+
+set -u pure_threshold_command_duration -1
+set -u pure_enable_git false
