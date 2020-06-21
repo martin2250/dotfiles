66f00d4c49e85031b7bc0ab4b62c7b7decfa29e0
Martin Pittermann <martinp2250@gmail.com>
2020-06-21 09:53:29 +0200

    add fish aliases

---
+++ dot_config/fish/executable_config.fish
@@ -0,0 +1,28 @@
+# change default behavior of shell utils
+alias ls='ls --color=auto'
+alias dd='dd status=progress'
+alias ddsync='dd status=progress conv=fdatasync'
+alias find='find 2>/dev/null'
+alias grep='grep --color=auto'
+alias sc='systemctl'
+
+# custom commands
+alias la='ls -thora'
+alias open='run xdg-open'
+alias pdfcombine='gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -sOutputFile='
+alias udevreload='sudo udevadm control --reload-rules; sudo udevadm trigger;'
+
+# cd to directory and list files (has to be here so ls alias is already available)
+function cdl
+	cd "$1";
+	ls;
+end
+
+# git aliases
+alias gca='git commit -a'
+alias gp='git pull --rebase'
+alias gpu='git push'
+
+# clipboard actions
+alias clipget='xclip -selection c -o'	# get clipboard to stdout
+alias clipset='xclip -selection c'		# set clipboard from stdin
