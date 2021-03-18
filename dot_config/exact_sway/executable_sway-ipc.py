4fe3c154e13c93a334410e29c58fd6a5f2fcf727
Martin Pittermann <martinp2250@gmail.com>
2021-03-18 23:21:53 +0100

    improve sway transparency

--- dot_config/exact_sway/executable_sway-ipc.py
+++ dot_config/exact_sway/executable_sway-ipc.py
@@ -3,12 +3,20 @@ import i3ipc
 
 ipc = i3ipc.Connection()
 
-def on_window_focus(ipc, e):
+def on_window_focus(ipc: i3ipc.Connection, e):
+    c : i3ipc.con.Con
     for c in ipc.get_tree():
-        if c.focused:
+        # only consider windows
+        if c.type != 'con':
+            continue
+        opaque = c.focused
+        opaque |= 'YouTube' in (c.name or '')
+        opaque |= 'Netflix' in (c.name or '')
+        opaque |= 'vlc' == c.window_class
+        if opaque:
             c.command('opacity 1')
         else:
             c.command('opacity 0.9')
 
 ipc.on("window::focus", on_window_focus)
-ipc.main() # enter command loop
+ipc.main()
