8631dddf714f03c74a1d84c000f0746be0aa10a1
Martin Pittermann <martinp2250@gmail.com>
2021-06-05 11:09:10 +0200

    changes from PC

--- dot_config/exact_picom/executable_launch.sh
+++ dot_config/exact_picom/executable_launch.sh
@@ -4,4 +4,4 @@ killall -q compton
 
 while pgrep -u $UID -x compton >/dev/null; do sleep 0.2; done
 
-compton --config ~/.config/compton/compton.conf -b &
+picom --config ~/.config/picom/picom.conf -b &
